1d37df1127714aaa8211551e168a0de59526514d
Nobuaki Ogawa <Nobuaki.Ogawa@efi.com>
2016-03-21 15:45:51 +0100

    Add Description for Deploy_Version parameter

--- JobDSL_DSMG_Deployment.groovy
+++ JobDSL_DSMG_Deployment.groovy
@@ -4,7 +4,7 @@ job('DSMG_Deployment' + CUSTOMER_NAME) {
         booleanParam('DSMG_Deploy', false, 'Uncheck - Check run script in console log, Check - Execute on the target')
 		stringParam('FQDN', FQDN,'Fully Qualified Domain Name of the DSMI website')
 		stringParam('DSMG_INSTALLDIR', 'C:\\Program Files (x86)\\DirectSmile Generator', 'Installation target directory for DSMOnline Backend')
-        choiceParam('Deploy_Version', ['DSMG_LATEST_RELEASE(default)', 		'DSMG_LATEST_STABLE_RELEASE', 'DSMG_DSF_RELEASE', 'DSMG_VER5_1_x', 'DSMG_VER5_4_x', 'DSMG_VER6_0_x', 'DSMG_TRUNK_VERSION', 'DSMG_SPECIFIC_VERSION'])
+        choiceParam('Deploy_Version', ['DSMG_LATEST_RELEASE(default)', 		'DSMG_LATEST_STABLE_RELEASE', 'DSMG_DSF_RELEASE', 'DSMG_VER5_1_x', 'DSMG_VER5_4_x', 'DSMG_VER6_0_x', 'DSMG_TRUNK_VERSION', 'DSMG_SPECIFIC_VERSION'], 'Select version you want to deploy')
 		stringParam('DSMG_VERSION_NUMBER', '5.1.0.367', 'You need to specify version number at here. File name should have "DSMGenInstaller-" as prefix, and ".msi" as postfix. So you only need to provide exact version number at here')	
     }
 }

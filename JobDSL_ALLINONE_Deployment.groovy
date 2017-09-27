d462f8124114309c4ab3e31e0a285158a791dfac
tylerbro <tylerbro@efi.com>
2017-09-27 14:46:00 -0400

    Update JobDSL_ALLINONE_Deployment.groovy

--- JobDSL_ALLINONE_Deployment.groovy
+++ JobDSL_ALLINONE_Deployment.groovy
@@ -179,20 +179,20 @@ job('DirectSmile__Deployment_'+ SERVERNAME) {
 	parameters {
 	//***************************************************************************
 	//******* General Arguments ********
-        booleanParam('DEBUG_RUN', false, '<p>If DEBUG_RUN=True all commands will be echoed to the screen only. Target system will not be touched. And it does not trigger downstrem jobs</p>')
-        booleanParam('DSMG_DEPLOY', false, '<p>If DSMG_DEPLOY=True, DSMG Deployment commands will be executed.</p><p>DSMG_DEPLOY=False, then Target system will not be touched.</p>')
-        booleanParam('DSMI_DEPLOY', false, '<p>If DSMI_DEPLOY=True, DSMI Deployment commands will be executed.</p><p>DSMI_DEPLOY=False, then Target system will not be touched.</p>')
-        booleanParam('DSMX_DEPLOY', false, '<p>If DSMX_DEPLOY=True, DSMX Deployment commands will be executed.</p><p>DSMX_DEPLOY=False, then Target system will not be touched.</p>')
-		stringParam('FQDN', FQDN ,'<h3>Fully Qualified Domain Name</h3><p>Customer server FQDN which listedn by DSMInstallation Service</p>')
+        	booleanParam('DEBUG_RUN', false, '<p>If DEBUG_RUN=True all commands will be echoed to the screen only. Target system will not be touched. And it does not trigger downstrem jobs</p>')
+        	booleanParam('DSMG_DEPLOY', false, '<p>If DSMG_DEPLOY=True, DSMG Deployment commands will be executed. If DSMG_DEPLOY=False, then Target system will not update DSMG component.</p>')
+        	booleanParam('DSMI_DEPLOY', false, '<p>If DSMI_DEPLOY=True, DSMI Deployment commands will be executed. If DSMI_DEPLOY=False, then Target system will not update DSMI component.</p>')
+        	booleanParam('DSMX_DEPLOY', false, '<p>If DSMX_DEPLOY=True, DSMX Deployment commands will be executed. If DSMX_DEPLOY=False, then Target system will not update DSMX component.</p>')
+		stringParam('FQDN', FQDN ,'<h3>Fully Qualified Domain Name</h3><p>Customer server FQDN which listened by DSMInstallation Service</p>')
 	//***************************************************************************
 	//******* Deployment Version selector
 	//***************************************************************************
-        choiceParam('DSMG_DEPLOY_VERSION',['DSMG_SPECIFIC_VERSION','DSMG_LATEST_RELEASE','DSMG_DSF_RELEASE'],'<h2>Select version you want to deploy</h2>')
-		choiceParam('DSMI_DEPLOY_VERSION', ['DSMI_SPECIFIC_VERSION','DSMI_LATEST_RELEASE','DSMI_DSF_RELEASE'],'<h2>Select version you want to deploy</h2>')
+        	choiceParam('DSMG_DEPLOY_VERSION',['DSMG_SPECIFIC_VERSION','DSMG_LATEST_RELEASE','DSMG_DSF_RELEASE'],'')
+		choiceParam('DSMI_DEPLOY_VERSION', ['DSMI_SPECIFIC_VERSION','DSMI_LATEST_RELEASE','DSMI_DSF_RELEASE'],'')
 		choiceParam('DSMX_DEPLOY_VERSION', ['DSMX_SPECIFIC_VERSION','DSMX_LATEST_RELEASE','DSMX_DSF_RELEASE'],'<h2>Select version you want to deploy</h2>')
-		stringParam('DSMG_INSTALLER_FILE_PATH', DSMG_INSTALLER_FILE_PATH, '<h3>Abosolute File Path or URL</h3><p>You can use local directory path as value in here, as well as UNC path is supported</p><ul> <li>Example input: (URL) <a href="http://directsmile.blob.core.windows.net/installer/dsmg.msi">http://directsmile.blob.core.windows.net/installer/dsmg.msi<br /></a></li> <li>Example input: (UNC)&nbsp; <a href="\\\\NetworkAccessStorage\\DirectSmile\\Installer\\dsmg.msi">\\\\NetworkAccessStorage\\DirectSmile\\Installer\\dsmg.msi</a></li> </ul>')
-		stringParam('DSMI_INSTALLER_FILE_PATH', DSMI_INSTALLER_FILE_PATH, '<h3>Abosolute File Path or URL</h3><p>You can use local directory path as value in here, as well as UNC path is supported</p><ul> <li>Example input: (URL) <a href="http://directsmile.blob.core.windows.net/installer/dsmi.msi">http://directsmile.blob.core.windows.net/installer/dsmi.msi<br /></a></li> <li>Example input: (UNC)&nbsp; <a href="\\\\NetworkAccessStorage\\DirectSmile\\Installer\\dsmi.msi">\\\\NetworkAccessStorage\\DirectSmile\\Installer\\dsmi.msi</a></li> </ul>')
-		stringParam('DSMX_INSTALLER_FILE_PATH', DSMX_INSTALLER_FILE_PATH, '<h3>Abosolute File Path or URL</h3><p>You can use local directory path as value in here, as well as UNC path is supported</p><ul> <li>Example input: (URL) <a href="http://directsmile.blob.core.windows.net/installer/dsmx.msi">http://directsmile.blob.core.windows.net/installer/dsmx.msi<br /></a></li> <li>Example input: (UNC)&nbsp; <a href="\\\\NetworkAccessStorage\\DirectSmile\\Installer\\dsmx.msi">\\\\NetworkAccessStorage\\DirectSmile\\Installer\\dsmx.msi</a></li> </ul>')
+		stringParam('DSMG_INSTALLER_FILE_PATH', DSMG_INSTALLER_FILE_PATH, '<h3>Abosolute File Path or URL</h3><p>You can use local directory path as value in here, as well as UNC path is supported')
+		stringParam('DSMI_INSTALLER_FILE_PATH', DSMI_INSTALLER_FILE_PATH, '<h3>Abosolute File Path or URL</h3><p>You can use local directory path as value in here, as well as UNC path is supported')
+		stringParam('DSMX_INSTALLER_FILE_PATH', DSMX_INSTALLER_FILE_PATH, '<h3>Abosolute File Path or URL</h3><p>You can use local directory path as value in here, as well as UNC path is supported')
 	//***************************************************************************
 	//******* DSMG Relates Arguments
 	//***************************************************************************
@@ -202,7 +202,6 @@ job('DirectSmile__Deployment_'+ SERVERNAME) {
 		booleanParam('ALLUSERS', ALLUSERS.toBoolean(), '<p>If ALLUSERS=True(Automatically convert to ALLUSERS=1 in actual deployment script), Register components for all users.</p>')
 		booleanParam('CLIENTUILEVEL', CLIENTUILEVEL.toBoolean(), '<p>If CLIENTUILEVEL=false(Automatically convert to CLIENTUILEVEL=0 in actual deployment script), Installation dialog visibility level would be changed</p>')
 		stringParam('MSILOG', MSILOG,'<h3><font color="red">Only Valid &gt; Ver1.0.0.94 of DSM Installation Service</font></h3></br><p>MSILOG="C:\\Program Files (x86)\\DirectSmile\\DirectSmile Logs\\DSMI_Deployment_MSILOG.log"</p></br></br><p>if NOT empty, the installer write msi log file by specified path/name</p></br>')
-
 	//***************************************************************************
 	//******* DSMI and DSMX common Arguments
 	//***************************************************************************
@@ -215,21 +214,21 @@ job('DirectSmile__Deployment_'+ SERVERNAME) {
 	//***************************************************************************
 	//******* DSMI and DSMX common Arguments - Set Default Credential ID
 	//***************************************************************************
-		//Replaced to Credential Plugin with Credential Binding Plugin model
+	//Replaced to Credential Plugin with Credential Binding Plugin model
 		booleanParam('CONFIGURE_IISAPPLICATIONPOOLIDENTITY_USER',CONFIGURE_IISAPPLICATIONPOOLIDENTITY_USER.toBoolean(),'Only available option higher than Ver6.1! Enable when you use specific user for Application Pool Identity')
 		credentialsParam('IISAPPLICATIONPOOLIDENTITY_CREDENTIAL') {
-            type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
-            defaultValue(IISAPPLICATIONPOOLIDENTITY_CREDENTIAL)
-            description('<p>Usually, you need to specify Domain Account which has enough privileges to access relevant directory and service</p>')
-        }
-		//Replaced to Credential Plugin with Credential Binding Plugin model
+            		type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
+            		defaultValue(IISAPPLICATIONPOOLIDENTITY_CREDENTIAL)
+            		description('<p>Usually, you need to specify Domain Account which has enough privileges to access relevant directory and service</p>')
+        	}
+	//Replaced to Credential Plugin with Credential Binding Plugin model
 		booleanParam('CONFIGURE_LOGINUSERFORBACKEND',CONFIGURE_LOGINUSERFORBACKEND.toBoolean(),'Only available option higher than Ver6.1! Enable when you use specific user as login of DSMOnline Backend to run it as Windows Service')
 		stringParam('SERVICE_DOMAIN',SERVICE_DOMAIN,'Service user domain that is executed')
 		credentialsParam('LOGINUSERFORBACKEND_CREDENTIAL') {
-            type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
-            defaultValue(LOGINUSERFORBACKEND_CREDENTIAL)
-            description('<h3>Login User for Backend</h3></br><p>In case you run DirectSmile as Service mode, please add new credential in here, then specify your created Credential in here</p>')
-        }
+            		type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
+            		defaultValue(LOGINUSERFORBACKEND_CREDENTIAL)
+            		description('<h3>Login User for Backend</h3></br><p>In case you run DirectSmile as Service mode, please add new credential in here, then specify your created Credential in here</p>')
+        	}
 	//***************************************************************************
 	//******* DSMI and DSMX common Arguments - SQL Database
 	//***************************************************************************
@@ -237,13 +236,13 @@ job('DirectSmile__Deployment_'+ SERVERNAME) {
 		stringParam('DSMI_SQLDATABASENAME',DSMI_SQLDATABASENAME,'Name of database for the DSMI-default, "dsmodb"')
 		stringParam('IMGDBNAME',IMGDBNAME,'Name of dsmoImages database*New from Ver7.0.0.53')
 		stringParam('DSMX_SQLDATABASENAME',DSMX_SQLDATABASENAME,'Name of database for the DSMX-default, "LP3_DSM"')
-		//Replaced to Credential Plugin with Credential Binding Plugin model
+	//Replaced to Credential Plugin with Credential Binding Plugin model
 		booleanParam('SQL_AUTHENTICATION',SQL_AUTHENTICATION.toBoolean(),'Does this server use SQL Authentication')
 		credentialsParam('SQL_CREDENTIAL') {
-            type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
-            defaultValue(SQL_CREDENTIAL)
-            description('<h3>SQL Authentication</h3></br><p>In case you use SQL Authentication, please add new credential in here, then specify your created Credential in here</p>')
-        }
+            		type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
+		        defaultValue(SQL_CREDENTIAL)
+            		description('<h3>SQL Authentication</h3></br><p>In case you use SQL Authentication, please add new credential in here, then specify your created Credential in here</p>')
+        	}
 	//***************************************************************************
 	//******* DSMIS Backup command common Arguments
 	//***************************************************************************
@@ -259,8 +258,8 @@ job('DirectSmile__Deployment_'+ SERVERNAME) {
 	//***************************************************************************
 		stringParam('DIRPROPERTY1', DIRPROPERTY1, 'UNC Path for the DSMI website directory')
 		stringParam('CNNAME', CNNAME,'Common Name that your SSL issued to')
-		stringParam('DSMGPATH',DSMGPATH,'<h3><font color="red">Only Valid &gt; Ver7.2.0.133</font></h3></br> <p>DSMGPATH="C:\\Program Files (x86)\\DirectSmile Generator"</p></br></br> <p>if NOT empty, the installer configures the DirectSmileWorkingDirectory in the tblSettings</p></br>')
-		stringParam('DSMXPATH',DSMXPATH,'<h3><font color="red">Only Valid &gt; Ver7.2.4</font></h3></br><p>DSMXPATH="C:\\inetpub\\wwwroot"</p></br></br><p>if NOT empty, the installer write input in DSMXRootPath field of dsmodb.tblSettings.</p></br>')
+		stringParam('DSMGPATH',DSMGPATH,'<h3><font color="red">Only Valid &gt; Ver7.2.0.133</font></h3></br> <p>DSMGPATH="C:\\Program Files (x86)\\DirectSmile Generator"</p></br> <p>if NOT empty, the installer configures the DirectSmileWorkingDirectory in the tblSettings</p></br>')
+		stringParam('DSMXPATH',DSMXPATH,'<h3><font color="red">Only Valid &gt; Ver7.2.4</font></h3></br><p>DSMXPATH="C:\\inetpub\\wwwroot"</p></br><p>if NOT empty, the installer write input in DSMXRootPath field of dsmodb.tblSettings.</p></br>')
 		stringParam('DSMUSERS',DSMUSERS,'<h3><font color="red">Only Valid &gt; Ver7.2.0.60</font></h3> <p>DSMusers directory is getting created and permissions set if DSMUSERS!=null</p>')
 		stringParam('DSMTEMP',DSMTEMP,'<h3><font color="red">Only Valid &gt; Ver7.2.0.54</font></h3> <p>DSMTemp can be set in installer now, creates also app pool and sets the permissions, sets the localimagerepository path and url in dsmodb.tblsettings</p>')
 		stringParam('DSMI_INSTALLDIR',DSMI_INSTALLDIR,'Install target directory for DSMOnline Backend')
@@ -275,7 +274,7 @@ job('DirectSmile__Deployment_'+ SERVERNAME) {
 		stringParam('IISAPPNAME',IISAPPNAME,'Optional Virtual directory name of the DSMI web application')
 		stringParam('SERVICE_EXE_NAME',SERVICE_EXE_NAME,'Optional Name of the DSMI Backend service application')
 		choiceParam('LOG_LEVEL', ['Information', 'Critical', 'Error', 'Resume', 'Start', 'Stop', 'Suspend', 'Transfer', 'Verbose', 'Warning'],'<h3><font color="red">Only Valid &gt; Ver7.1.0.40</font></h3> <p>LOG_LEVEL install param to configure loglevel (default=Information)</p> <table cellspacing="1" summary="TraceEventType"><tbody><tr><th>TraceEventType</th><th>Description</th></tr><tr><td>Critical</td><td>Critical Error or Application Crash</td></tr><tr><td>Error</td><td>Recoverable Error</td></tr><tr><td>Information</td><td>Information message</td></tr><tr><td>Resume</td><td>Resume processing</td></tr><tr><td>Start</td><td>Start processing</td></tr><tr><td>Stop</td><td>Stop processing</td></tr><tr><td>Suspend</td><td>Suspend processing</td></tr><tr><td>Transfer</td><td>Change relative ID</td></tr><tr><td>Verbose</td><td>Debug tracing</td></tr><tr><td>Warning</td><td>Not important issues</td></tr></tbody> </table>')
-		choiceParam('ENABLEWF', ['1','0'],'<h3><font color="red">Only Valid &gt; Ver7.2.0.52</font></h3> <p>ENABLEWF=1|0 enables the Workflow(1) or disables it (0), default is ENABLEWF=1</p>')
+		choiceParam('ENABLEWF', ENABLEWF,'<h3><font color="red">Only Valid &gt; Ver7.2.0.52</font></h3> <p>ENABLEWF=1|0 enables the Workflow(1) or disables it (0), default is ENABLEWF=1</p>')
 		stringParam('DSMI_MSILOG', DSMI_MSILOG,'<h3><font color="red">Only Valid &gt; Ver1.0.0.94 of DSM Installation Service</font></h3></br><p>MSILOG="C:\\Program Files (x86)\\DirectSmile\\DirectSmile Logs\\DSMI_Deployment_MSILOG.log"</p></br></br><p>if NOT empty, the installer write msi log file by specified path/name</p></br>')
 	//***************************************************************************
 	//******* DSMX Relates Arguments
